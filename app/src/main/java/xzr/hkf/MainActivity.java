e3d39ec1b3fea656e696b3c47f02629df06469bc
fukiame <fukiame@proton.me>
2026-01-14 00:26:14 +0700

    2 sta
    Signed-off-by: fukiame <fukiame@proton.me>

--- app/src/main/java/xzr/hkf/MainActivity.java
+++ app/src/main/java/xzr/hkf/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.WindowInsets;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -20,6 +21,7 @@ import androidx.core.view.ViewGroupCompat;
 import androidx.activity.OnBackPressedCallback;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.elevation.SurfaceColors;
 
 public class MainActivity extends AppCompatActivity {
     static final boolean DEBUG = false;
@@ -45,9 +47,17 @@ public class MainActivity extends AppCompatActivity {
         scrollView = new ScrollView(this);
         logView = new TextView(this);
 
+        getWindow().getDecorView().setOnApplyWindowInsetsListener((v, insets) -> {
+            android.graphics.Insets statusBar = insets.getInsets(WindowInsets.Type.systemBars());
+            v.setPadding(0, -statusBar.top, 0, 0);
+            v.setBackgroundColor(SurfaceColors.SURFACE_5.getColor(this));
+            // ViewGroupCompat.installCompatInsetsDispatch(v);
+            return insets;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(scrollView, (v, insets) -> {
             Insets bars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(bars.left, bars.top, bars.right, bars.bottom);
+            v.setPadding(bars.left, 0, bars.right, bars.bottom);
             ViewGroupCompat.installCompatInsetsDispatch(v);
             return WindowInsetsCompat.CONSUMED;
         });
